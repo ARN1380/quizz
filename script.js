dffe532702295d51caedc5220818e826dce5420c
ARN <alireza.naghavi0@gmail.com>
2023-02-15 16:19:48 +0330

    one question is handeld and ok to use

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ async function fetchQuizz() {
     console.log(quizz.results);
 }
 fetchQuizz();
+let clickedAnswerClass;
+let correctAnswer;
+const answer0 = document.querySelector(".answer0")
+const answer1 = document.querySelector(".answer1")
+const answer2 = document.querySelector(".answer2")
+const answer3 = document.querySelector(".answer3")
+const index = random(0,3);
+let answers = [answer0,answer1,answer2,answer3];
 
 function test (quizz) {
     const question = document.querySelector(".question");
@@ -13,10 +21,9 @@ function test (quizz) {
     
     //answers section ->
     const type = quizz.results[0].type
-    const answer0 = document.querySelector(".answer0")
-    const answer1 = document.querySelector(".answer1")
-    const answer2 = document.querySelector(".answer2")
-    const answer3 = document.querySelector(".answer3")
+    
+    console.log("answer3: "+answer3);
+
 
     if (type == "boolean") {
         if (random(0,1)) {
@@ -30,20 +37,36 @@ function test (quizz) {
         answer3.classList.add("answer-display-none")
         
     } else {
-        const answers = [answer0,answer1,answer2,answer3];
-        const index = random(0,3);
 
         answers[index].textContent = quizz.results[0].correct_answer;
         // answers[index].classList.add("test");
-        answers.splice(index,1)        
+        correctAnswer = answers.splice(index,1);
+        console.log(correctAnswer);
+        console.log(answers);
         answers[0].textContent = quizz.results[0].incorrect_answers[0];
         answers[1].textContent = quizz.results[0].incorrect_answers[1];
         answers[2].textContent = quizz.results[0].incorrect_answers[2];        
     }
+
 }
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+function clickedAnswer (e) {
+    let itemClicked = e.srcElement || e.target;   
+    clickedAnswerClass = itemClicked.className;
+    
+    
+    console.log( );
+    console.log(clickedAnswerClass + " == " + answers[index].className);
+    if (clickedAnswerClass == answers[index].className) {
+        
+        answers[index].classList.add("test")
 
+    } else {
+        console.log("wrong Answer!");
+    }
+
+}
